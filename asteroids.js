80e236609ed6e421d91ce7c4d57e9cf2c5d484af
yago santos <yagolcs@msn.com>
2021-09-24 17:01:34 -0300

    Asteroids and bullets.

--- asteroids.js
+++ asteroids.js
@@ -2,7 +2,10 @@ let canvas;
 let ctx;
 let canvasWidth = 1400;
 let canvasHeight = 1000;
+let ship;
 let keys = [];
+let bullets = [];
+let asteroids = [];
 document.addEventListener('DOMContentLoaded', SetupCanvas);
 
 function SetupCanvas(){
@@ -13,6 +16,9 @@ function SetupCanvas(){
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ship = new Ship();
+  for(let i = 0; i < 8; i++){
+    asteroids.push(new Asteroid());
+  }
   document.body.addEventListener("keydown", HandleKeyDown);
   document.body.addEventListener("keyup", HandleKeyUp);
   Render();
@@ -24,6 +30,9 @@ function HandleKeyDown(e){
 
 function HandleKeyUp(e){
   keys[e.keyCode] = false;
+  if (e.keyCode === 32){
+    bullets.push(new Bullet(ship.angle));
+  }
 }
 
 class Ship {
@@ -39,6 +48,8 @@ class Ship {
     this.radius = 15;
     this.angle = 0;
     this.strokeColor = 'white';
+    this.noseX = canvasWidth / 2 + 15;
+    this.noseY = canvasHeight / 2;
   }
 
   Rotate(dir) {
@@ -75,7 +86,75 @@ class Ship {
     ctx.beginPath();
     let vertAngle = ((Math.PI * 2) / 3);
     let radians = this.angle / Math.PI * 180;
+    this.noseX = this.x - this.radius * Math.cos(radians);
+    this.noseY = this.y - this.radius * Math.sin(radians);
     for (let i = 0; i < 3; i++) {
+      ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians), this.y - this.radius * Math.sin(vertAngle * i + radians));
+    }
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+
+class Bullet{
+  constructor(angle) {
+    this.visible = true;
+    this.x = ship.noseX;
+    this.y = ship.noseY;
+    this.angle = angle;
+    this.height = 4;
+    this.width = 4;
+    this.speed = 5;
+    this.velX = 0;
+    this.velY = 0;
+  }
+
+  Update(){
+    let radians = this.angle / Math.PI * 180;
+    this.x -= Math.cos(radians) * this.speed;
+    this.y -= Math.sin(radians) * this.speed;
+  }
+
+  Draw(){
+    ctx.fillStyle = 'white';
+    ctx.fillRect(this.x,this.y,this.width,this.height);
+  }
+}
+
+class Asteroid{
+  constructor(x,y) {
+    this.visible = true;
+    this.x = Math.floor(Math.random() * canvasWidth);
+    this.y = Math.floor(Math.random() * canvasHeight);
+    this.speed = 3;
+    this.radius = 50;
+    this.angle = Math.floor(Math.random() * 359);
+    this.strokeColor = 'white';
+  }
+
+  Update(){
+    let radians = this.angle / Math.PI * 180;
+    this.x += Math.cos(radians) * this.speed;
+    this.y += Math.sin(radians) * this.speed;
+    if (this.x < this.radius) {
+      this.x = canvas.width;
+    }
+    if (this.x > canvas.width) {
+      this.x = this.radius;
+    }
+    if (this.y < this.radius) {
+      this.y = canvas.height;
+    }
+    if (this.y > canvas.height) {
+      this.y = this.radius;
+    }
+  }
+
+  Draw(){
+    ctx.beginPath();
+    let vertAngle = ((Math.PI * 2) / 6);
+    var radians = this.angle / Math.PI * 180;
+    for(let i = 0; i < 6; i++){
       ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians), this.y - this.radius * Math.sin(vertAngle * i + radians));
     }
     ctx.closePath();
@@ -94,5 +173,17 @@ function Render() {
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
   ship.Update();
   ship.Draw();
+  if (bullets.length !== 0) {
+    for(let i = 0; i < bullets.length; i++){
+      bullets[i].Update();
+      bullets[i].Draw();
+    }
+  }
+  if (asteroids.length !== 0) {
+    for(let j = 0; j < asteroids.length; j++){
+      asteroids[j].Update();
+      asteroids[j].Draw(j);
+    }
+  }
   requestAnimationFrame(Render);
 }
